ad4377ade36a3f5b1cbe41f1a0778aa9dc2f648d
Hammad Ahmed <xammadaxmed@gmail.com>
2023-02-20 19:57:22 +0500

    Added New UI and its assets

--- Helpers/Uploader.cs
+++ Helpers/Uploader.cs
@@ -3,28 +3,28 @@
     public class Uploader
     {
         public static IWebHostEnvironment? Environment { get; set; }
-        public string Upload()
-        {
-            string wwwPath = Environment.WebRootPath;
-            string contentPath = Environment.ContentRootPath;
+        //public string Upload()
+        //{
+        //    string wwwPath = Environment.WebRootPath;
+        //    string contentPath = Environment.ContentRootPath;
 
-            string path = Path.Combine(Environment.WebRootPath, "Uploads");
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
+        //    string path = Path.Combine(Environment.WebRootPath, "Uploads");
+        //    if (!Directory.Exists(path))
+        //    {
+        //        Directory.CreateDirectory(path);
+        //    }
 
-            List<string> uploadedFiles = new List<string>();
-            foreach (IFormFile postedFile in postedFiles)
-            {
-                string fileName = Path.GetFileName(postedFile.FileName);
-                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
-                {
-                    postedFile.CopyTo(stream);
-                    uploadedFiles.Add(fileName);
-                    ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
-                }
-            }
-        }
+        //    List<string> uploadedFiles = new List<string>();
+        //    foreach (IFormFile postedFile in postedFiles)
+        //    {
+        //        string fileName = Path.GetFileName(postedFile.FileName);
+        //        using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+        //        {
+        //            postedFile.CopyTo(stream);
+        //            uploadedFiles.Add(fileName);
+        //            ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
+        //        }
+        //    }
+        //}
     }
 }
